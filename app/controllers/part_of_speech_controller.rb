afd4432102b48a62b41e5df405f7debe21f57016
CoralineAda <coraline@idolhands.com>
2015-01-01 17:08:43 -0600

    Ability to add forms to roots.

--- app/controllers/part_of_speech_controller.rb
+++ app/controllers/part_of_speech_controller.rb
@@ -1,7 +1,22 @@
 class PartOfSpeechController < ApplicationController
 
   def edit
-    @part_of_speech = ::PartOfSpeech.new(base_form: 'foo')
+    @part_of_speech = ::PartOfSpeech.new(base_form: 'word', root_word: params[:root_base_form])
+  end
+
+  def update
+    generic = PartOfSpeech.new(pos_params)
+    if generic.save
+      redirect_to generic.root
+    else
+      flash[:notice] = "The root could not be added."
+    end
+  end
+
+  private
+
+  def pos_params
+    params.require(:part_of_speech).permit!#(:base_form, :type, :root_word, :properties => [])
   end
 
 end
--- app/models/part_of_speech.rb
+++ app/models/part_of_speech.rb
@@ -4,28 +4,34 @@ class PartOfSpeech
   extend  ActiveModel::Naming
 
   attr_accessor :base_form
-  attr_accessor :form
-  attr_accessor :is_physical
+  attr_accessor :root_word
+  attr_accessor :type
+  attr_accessor :properties
 
   FORMS = %w{ adjective adverb noun verb }
+  ADJECTIVE_ATTRIBUTES = %w{ is_physical }
+  ALL_ATTRIBUTES = ADJECTIVE_ATTRIBUTES
 
-  def initialize(base_form:)
-    self.base_form = base_form
+  def initialize(params={})
+    self.base_form = params[:base_form]
+    self.root_word = params[:root_word]
+    self.properties = params[:properties]
+    self.type = params[:type]
   end
 
   def save
-    object = klass.create(base_form: self.base_form)
-    object.is_physical = self.is_physical if object.respond_to?(:is_physical)
-  end
-
-  def klass
-    case self.form
-      when 'adjective'; Gramercy::PartOfSpeech::Adjective
-    end
+    object = Gramercy::PartOfSpeech::Generic.create!(base_form: self.base_form, type: self.type)
+    object.set_root(root)
+    self.properties.each{ |k,v| object.set_property(k,v) }
+    object.save
   end
 
   def id
     1
+  end
+
+  def root
+    @root ||= Gramercy::Meta::Root.find_by(base_form: self.root_word)
   end
 
   def persisted?
